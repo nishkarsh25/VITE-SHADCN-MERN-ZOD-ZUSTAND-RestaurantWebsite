fde043a523d2602e51e37714d710c11fd5d507b1
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 03:30:20 +0530

    Component Definition:
    State Management:
    
    input State: Manages the form input fields (fullname, email, password, contact). Initialized with empty strings.
    errors State: Stores validation error messages for each field. Initialized as an empty object.
    Hook Usage:
    
    useUserStore Hook: Provides access to the signup function and loading state from the user store.
    useNavigate Hook: Provides navigation functionality to redirect users after successful signup.

--- client/src/auth/Signup.tsx
+++ client/src/auth/Signup.tsx
@@ -7,5 +7,21 @@ import { Loader2, LockKeyhole, Mail, PhoneOutgoing, User } from "lucide-react";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
+// typescript me type define krne ka 2 trika hota hai
+
+const Signup = () => {
+    const [input, setInput] = useState<SignupInputState>({
+        fullname:"",
+        email:"",
+        password:"", 
+        contact:"", 
+    });
+    const [errors, setErrors] = useState<Partial<SignupInputState>>({});
+    const {signup, loading} = useUserStore();
+const navigate = useNavigate();
+    
+  
+  
+};
 
 
