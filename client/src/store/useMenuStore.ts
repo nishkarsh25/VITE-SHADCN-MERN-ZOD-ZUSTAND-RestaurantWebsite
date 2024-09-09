8d781ab4cb01491e94c272149f366382d50f73ae
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 02:58:33 +0530

    API Endpoint Configuration: API_END_POINT: This constant holds the base URL for all menu-related API requests (http://localhost:8000/api/v1/menu).

--- client/src/store/useMenuStore.ts
+++ client/src/store/useMenuStore.ts
@@ -4,7 +4,8 @@ import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 import { useRestaurantStore } from "./useRestaurantStore";
 
-
+const API_END_POINT = "http://localhost:8000/api/v1/menu";
+axios.defaults.withCredentials = true;
 
 
 
