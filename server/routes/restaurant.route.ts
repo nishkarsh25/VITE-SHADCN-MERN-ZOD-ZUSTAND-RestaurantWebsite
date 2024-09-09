3679834213829c76b428d1fe30e8fb6392faba18
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-09 23:13:15 +0530

    Imports express: The core Express library used to create the router. createRestaurant, getRestaurant, getRestaurantOrder, getSingleRestaurant, searchRestaurant, updateOrderStatus, updateRestaurant: Controller functions imported from restaurant.controller to handle various restaurant-related actions. upload: Middleware imported from middlewares/multer for handling file uploads. isAuthenticated: Middleware imported from middlewares/isAuthenticated to ensure routes are accessible only to authenticated users.

---
+++ server/routes/restaurant.route.ts
@@ -0,0 +1,13 @@
+import express from "express"
+import { createRestaurant, getRestaurant, getRestaurantOrder, getSingleRestaurant, searchRestaurant, updateOrderStatus, updateRestaurant } from "../controller/restaurant.controller";
+import upload from "../middlewares/multer";
+import {isAuthenticated} from "../middlewares/isAuthenticated";
+
+
+
+
+
+
+
+
+
