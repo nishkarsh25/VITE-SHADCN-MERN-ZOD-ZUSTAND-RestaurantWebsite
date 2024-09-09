63c53f137361f7b60e8a54c04615efb52bce3465
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-09 23:14:36 +0530

    Router Creation const router = express.Router();: Creates an instance of an Express router. This router will manage HTTP requests for order-related routes.

--- server/routes/order.route.ts
+++ server/routes/order.route.ts
@@ -1,7 +1,7 @@
 import express from "express"
 import {isAuthenticated} from "../middlewares/isAuthenticated";
 import { createCheckoutSession, getOrders, stripeWebhook } from "../controller/order.controller";
-
+const router = express.Router();
 
 
 
