ab7da7f2a611423efd13ea7eb452c2d1f499e3d5
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-09 23:09:00 +0530

    Imports and Configuration Request and Response: These types from Express define the shape of request and response objects in your route handlers. Restaurant and Order: Mongoose models for managing restaurant and order data. Stripe: The Stripe library for handling payments. It's initialized with a secret key from environment variables.

---
+++ server/controller/order.controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import { Restaurant } from "../models/restaurant.model";
+import { Order } from "../models/order.model";
+import Stripe from "stripe";
+
+
+
+
+
+
+
+
+
+
+
