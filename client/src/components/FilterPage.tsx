9509c643955f3060cb34982bb7fdabdc4698879d
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 04:31:53 +0530

    Types FilterOptionsState: Defines the structure of the filter options, with an id and a label for each filter item. This type ensures that filter options are consistently formatted.

--- client/src/components/FilterPage.tsx
+++ client/src/components/FilterPage.tsx
@@ -3,7 +3,10 @@ import { Button } from "./ui/button";
 import { Checkbox } from "./ui/checkbox";
 import { Label } from "./ui/label";
 
-
+export type FilterOptionsState = {
+  id: string;
+  label: string;
+};
 
 
 
