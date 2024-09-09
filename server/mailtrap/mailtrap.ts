859b5ceeb3935e89e3b50ff61e15ef038e364c6d
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-09 22:54:58 +0530

    Mailtrap Client Configuration: export const client = new MailtrapClient({ token: process.env.MAILTRAP_API_TOKEN! });: Creates an instance of MailtrapClient using the Mailtrap API token. process.env.MAILTRAP_API_TOKEN!: Retrieves the Mailtrap API token from the environment variables. The exclamation mark (!) is a TypeScript non-null assertion operator, indicating that the value is expected to be non-null and defined. export const client: Exports the client instance, making it available for use in other parts of the application. 4. Sender Details: export const sender = { email: "mailtrap@demomailtrap.com", name: "Patel MernStack" };: Defines and exports an object containing the sender's email and name. email: "mailtrap@demomailtrap.com": Specifies the email address used as the sender for outgoing emails. name: "Patel MernStack": Specifies the name associated with the sender's email address. Summary: The code configures a Mailtrap client for sending emails by importing the necessary classes, loading environment variables for secure configuration, and defining the sender's details. The client instance is used to interact with the Mailtrap service, and the sender object specifies the email address and name that will appear as the sender of the emails.

--- server/mailtrap/mailtrap.ts
+++ server/mailtrap/mailtrap.ts
@@ -3,5 +3,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
  
+export const client = new MailtrapClient({token: process.env.MAILTRAP_API_TOKEN! });
 
-
+export const sender = {
+  email: "mailtrap@demomailtrap.com",
+  name: "Patel MernStack",
+};
