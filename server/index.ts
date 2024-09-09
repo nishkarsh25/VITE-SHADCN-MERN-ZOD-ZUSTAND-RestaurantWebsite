015f95d3646028d3c215ab9cdd50afef99b38d26
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-09 23:16:46 +0530

    Imports express: The core Express library used to create the server and manage routes. dotenv: A library to load environment variables from a .env file into process.env. connectDB: A custom function to connect to a MongoDB database (imported from ./db/connectDB). bodyParser: Middleware for parsing incoming request bodies, though express.json() and express.urlencoded() cover similar functionality. cookieParser: Middleware for parsing cookies from request headers. cors: Middleware to enable Cross-Origin Resource Sharing (CORS) for handling requests from different origins. userRoute, restaurantRoute, menuRoute, orderRoute: Route modules for handling user, restaurant, menu, and order-related API requests, respectively. path: A utility module for working with file and directory paths.

---
+++ server/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/connectDB";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import userRoute from "./routes/user.route";
+import restaurantRoute from "./routes/restaurant.route";
+import menuRoute from "./routes/menu.route";
+import orderRoute from "./routes/order.route";
+import path from "path";
+
+
+
+
+
+
+
+
+
+
+
