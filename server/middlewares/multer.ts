958e203b360b1b495e595099cadbe6454e7b28d3
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-09 22:49:47 +0530

    Configuring Multer: const upload = multer({: This creates a multer instance with specific configuration options. storage: storage,: Sets the storage option to the previously defined memoryStorage. This means the uploaded files will be stored in memory. limits: { fileSize: 5 * 1024 * 1024 }, // 5mb: The limits option is used to specify constraints on the uploaded files. fileSize: 5 * 1024 * 1024 sets a limit of 5 megabytes (5MB) on the size of each uploaded file. If a file exceeds this size, multer will reject the upload, and an error will be generated.

--- server/middlewares/multer.ts
+++ server/middlewares/multer.ts
@@ -1,3 +1,8 @@
 import multer from "multer"
 const storage = multer.memoryStorage();
-
+const upload  = multer({
+    storage:storage,
+    limits:{
+        fileSize: 5*1024*1024 ,// 5mb
+    }
+})
