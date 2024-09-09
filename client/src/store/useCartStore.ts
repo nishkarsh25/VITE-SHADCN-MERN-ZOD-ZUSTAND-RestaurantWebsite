01aadaee5d9861b37e22b05eca06a3d6b5884490
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 03:00:00 +0530

    Imports: CartState and MenuItem Types: CartState represents the structure of the cart’s state (likely includes a list of items and other properties). MenuItem is the structure of an item that can be added to the cart, such as a dish from a restaurant menu. zustand: Zustand is a state management library. It is used here to create a global state store (useCartStore) that can be accessed anywhere in the app. zustand/middleware: Middleware functions like persist and createJSONStorage are used to persist the store state (the cart data) into localStorage, ensuring that the cart data is not lost on page reload.

---
+++ client/src/store/useCartStore.ts
@@ -0,0 +1,6 @@
+import { CartState } from "@/types/cartType";
+import { MenuItem } from "@/types/restaurantType";
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+
