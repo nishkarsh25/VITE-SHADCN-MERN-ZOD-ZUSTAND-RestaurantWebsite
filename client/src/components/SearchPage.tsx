6fdb3ab5bc3b4decf9a55413dcfe6227f45e41e1
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 04:24:03 +0530

    Overview The SearchPage component is designed to facilitate the search for restaurants based on user input and display the results. It leverages React hooks and components to manage the state and render the UI. It integrates with a state management store (useRestaurantStore) to handle the search logic and store data. It also includes components for displaying skeleton loaders while data is being fetched and handling scenarios where no search results are found.

---
+++ client/src/components/SearchPage.tsx
@@ -0,0 +1,18 @@
+import { Link, useParams } from "react-router-dom";
+import FilterPage from "./FilterPage";
+import { Input } from "./ui/input";
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+import { Badge } from "./ui/badge";
+import { Globe, MapPin, X } from "lucide-react";
+import { Card, CardContent, CardFooter } from "./ui/card";
+import { AspectRatio } from "./ui/aspect-ratio";
+import { Skeleton } from "./ui/skeleton";
+import { useRestaurantStore } from "@/store/useRestaurantStore";
+import { Restaurant } from "@/types/restaurantType";
+
+
+
+
+
+
