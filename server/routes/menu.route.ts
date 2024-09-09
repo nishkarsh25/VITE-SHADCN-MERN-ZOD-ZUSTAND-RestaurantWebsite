cb5a9bb335cfa4a39c527814f30d30e7db8d8b84
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-09 23:15:52 +0530

    Route Definitions / (POST):
    Middleware: isAuthenticated (ensures that the request comes from an authenticated user).
    Middleware: upload.single("image") (handles file uploads for a single image with the field name "image").
    Controller Function: addMenu (handles the creation of a new menu item. This function expects an image file to be uploaded along with the menu details).
    /:id (PUT):
    
    Middleware: isAuthenticated (ensures that the request comes from an authenticated user).
    Middleware: upload.single("image") (handles file uploads for updating a menu item’s image. The field name for the image is "image").
    Controller Function: editMenu (handles the update of an existing menu item identified by the id parameter. This function can update details and replace the image of the menu item).

--- server/routes/menu.route.ts
+++ server/routes/menu.route.ts
@@ -5,7 +5,8 @@ import { addMenu, editMenu } from "../controller/menu.controller";
 
 const router = express.Router();
 
-
+router.route("/").post(isAuthenticated, upload.single("image"), addMenu);
+router.route("/:id").put(isAuthenticated, upload.single("image"), editMenu);
  
 
 
