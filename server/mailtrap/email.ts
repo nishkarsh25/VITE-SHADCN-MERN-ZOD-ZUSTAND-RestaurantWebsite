e5222b4f5c8eaa0c230f438120b51f8d04a548c4
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-09 22:56:17 +0530

    Email Sending Functions: 1. sendVerificationEmail: Purpose: Sends an email to verify the user's email address. Parameters: email: The recipient's email address. verificationToken: A token used to verify the user's email. Process: recipient: An array containing the recipient's email address. htmlContent: Uses the imported htmlContent template and replaces the placeholder {verificationToken} with the actual token. Mailtrap send Method: Sends an email with the subject 'Verify your email' and includes the modified HTML content. Error Handling: Catches and logs errors, throwing a new error with a message if email sending fails.

--- server/mailtrap/email.ts
+++ server/mailtrap/email.ts
@@ -1,6 +1,21 @@
 import { generatePasswordResetEmailHtml, generateResetSuccessEmailHtml, generateWelcomeEmailHtml, htmlContent } from "./htmlEmail";
 import { client, sender } from "./mailtrap";
 
+export const sendVerificationEmail = async (email: string, verificationToken: string) => {
+    const recipient = [{ email }];
+    try {
+        const res = await client.send({
+            from: sender,
+            to: recipient,
+            subject: 'Verify your email',
+            html:htmlContent.replace("{verificationToken}", verificationToken),
+            category: 'Email Verification'
+        });
+    } catch (error) {
+        console.log(error);
+        throw new Error("Failed to send email verification")
 
+    }
+}
 
 
