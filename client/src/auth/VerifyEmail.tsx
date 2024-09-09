b7fc373dba674933e8fc8184aa8295cee909562f
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 03:39:23 +0530

    Handlers:
    handleChange:
    
    Triggered when the value of an input field changes.
    Validates that the entered value is either an alphanumeric character or empty.
    Updates the OTP state with the new value.
    Automatically moves focus to the next input field if a digit is entered, except for the last input.
    handleKeyDown:
    
    Triggered on keydown events for input fields.
    Specifically handles the "Backspace" key to move focus to the previous input field if the current field is empty and the user is deleting a character.
    submitHandler:
    
    Triggered when the form is submitted.
    Prevents the default form submission behavior.
    Joins the OTP array into a single string and attempts to verify the email using the verifyEmail function from useUserStore.
    Navigates to the home page upon successful verification. Logs errors to the console if verification fails.

--- client/src/auth/VerifyEmail.tsx
+++ client/src/auth/VerifyEmail.tsx
@@ -10,7 +10,35 @@ const VerifyEmail = () => {
   const inputRef = useRef<any>([]);
   const { loading, verifyEmail } = useUserStore();
   const navigate = useNavigate();
-  
+  const handleChange = (index: number, value: string) => {
+    if (/^[a-zA-Z0-9]$/.test(value) || value === "") {
+      const newOtp = [...otp];
+      newOtp[index] = value;
+      setOtp(newOtp);
+    }
+    // Move to the next input field id a digit is entered
+    if (value !== "" && index < 5) {
+      inputRef.current[index + 1].focus();
+    }
+  };
+
+  const handleKeyDown = (
+    index: number,
+    e: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (e.key === "Backspace" && !otp[index] && index > 0) {
+      inputRef.current[index - 1].focus();
+    }
+  };
+  const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const verificationCode = otp.join("");
+    try {
+      await verifyEmail(verificationCode);
+      navigate("/");
+    } catch (error) {console.log(error);
+    }
+  };
 
   
 };
