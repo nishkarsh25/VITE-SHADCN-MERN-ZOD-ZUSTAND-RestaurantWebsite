568fdaf18bf28afe72abf939e2f1d2a28493c0c0
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-09 23:15:41 +0530

    Router Creation const router = express.Router();: Creates an instance of an Express router. This router will handle HTTP requests related to menu management.

--- server/routes/menu.route.ts
+++ server/routes/menu.route.ts
@@ -3,7 +3,7 @@ import upload from "../middlewares/multer";
 import {isAuthenticated} from "../middlewares/isAuthenticated";
 import { addMenu, editMenu } from "../controller/menu.controller";
 
-
+const router = express.Router();
 
 
  
