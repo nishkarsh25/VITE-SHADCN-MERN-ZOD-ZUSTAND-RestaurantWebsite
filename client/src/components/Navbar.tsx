09063dc8f2959ff285950886035f8eefc5fccfcd
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 04:13:48 +0530

    Imports: Link: From react-router-dom, used to create navigable links in the application. UI Components: Several custom UI components like Menubar, DropdownMenu, Button, Avatar, Sheet, and Separator are imported. These are used to build and style different parts of the navbar. Icons: Imported from lucide-react, these are used for various icons in the UI (e.g., ShoppingCart, User, Sun, Moon, etc.). State Management: Hooks from custom stores (useUserStore, useCartStore, useThemeStore) are used to manage user information, cart items, and theme settings.

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,47 @@
+import { Link } from "react-router-dom";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from "./ui/menubar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+import {
+  HandPlatter,
+  Loader2,
+  Menu,
+  Moon,
+  PackageCheck,
+  ShoppingCart,
+  SquareMenu,
+  Sun,
+  User,
+  UtensilsCrossed,
+} from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Separator } from "./ui/separator";
+import { useUserStore } from "@/store/useUserStore";
+import { useCartStore } from "@/store/useCartStore";
+import { useThemeStore } from "@/store/useThemeStore";
+
+
+
+
+
