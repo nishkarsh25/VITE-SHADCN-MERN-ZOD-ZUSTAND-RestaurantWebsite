fb8fbb7ef631a7b34033a7bc9b7ce5dc2edda531
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 04:34:16 +0530

    Component Structure Props:
    menus:
    An array of MenuItem objects passed as a prop. Each MenuItem represents a menu option available at the restaurant.
    State and Handlers:
    
    addToCart:
    A function from useCartStore that adds a menu item to the shopping cart. It is invoked when a user clicks the "Add to Cart" button.
    navigate:
    A function from useNavigate that programmatically navigates the user to the /cart route when an item is added to the cart.

--- client/src/components/AvailableMenu.tsx
+++ client/src/components/AvailableMenu.tsx
@@ -4,6 +4,10 @@ import { Card, CardContent, CardFooter } from "./ui/card";
 import { useCartStore } from "@/store/useCartStore";
 import { useNavigate } from "react-router-dom";
 
-
+const AvailableMenu = ({ menus }: { menus: MenuItem[] }) => {
+  const { addToCart } = useCartStore();
+  const navigate = useNavigate();
+  
+};
 
 
