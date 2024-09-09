3c63d60eb0b3a2e5610d049638639de55d044552
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 03:29:53 +0530

    Component Setup: Imports:
    UI Components: Imports UI components like Button, Input, and Separator from a custom component library for consistent styling and functionality.
    Schema and Types: Imports types and validation schemas from a user schema file to manage and validate form data.
    State Management: Imports the useUserStore hook to manage user-related state and actions, such as signup.
    Icons: Imports icons from lucide-react for visual representation of form fields.
    React Hooks: Imports React hooks (useState, useNavigate) for managing component state and navigation.

---
+++ client/src/auth/Signup.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { SignupInputState, userSignupSchema } from "@/schema/userSchema";
+import { useUserStore } from "@/store/useUserStore";
+import { Loader2, LockKeyhole, Mail, PhoneOutgoing, User } from "lucide-react";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+
+
