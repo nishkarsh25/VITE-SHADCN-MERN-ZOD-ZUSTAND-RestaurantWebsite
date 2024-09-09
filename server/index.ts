94053385805ab139f64123b9b93f0fdcd827bdc9
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-09 23:17:45 +0530

    Static Files app.use(express.static(path.join(DIRNAME, "/client/dist")));: Serves static files (e.g., HTML, CSS, JavaScript) from the client/dist directory. This directory is expected to contain the frontend build files.
    app.use("*", (_, res) => { res.sendFile(path.resolve(DIRNAME, "client", "dist", "index.html")); });: For any request that doesn’t match a defined route, this serves the index.html file from the client/dist directory. This is often used for single-page applications (SPAs) to handle client-side routing.

--- server/index.ts
+++ server/index.ts
@@ -35,5 +35,8 @@ app.use("/api/v1/restaurant", restaurantRoute);
 app.use("/api/v1/menu", menuRoute);
 app.use("/api/v1/order", orderRoute);
 
-
+app.use(express.static(path.join(DIRNAME,"/client/dist")));
+app.use("*",(_,res) => {
+    res.sendFile(path.resolve(DIRNAME, "client","dist","index.html"));
+});
 
