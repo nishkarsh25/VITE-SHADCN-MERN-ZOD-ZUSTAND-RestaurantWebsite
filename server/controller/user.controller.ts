fb34744010b87174ee6772a13aea7255a6e32964
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-09 23:01:17 +0530

    Reset Password Function: This function handles the actual password reset process.
    Token Validation: The function checks if the reset token in the request parameters matches a user and if the token hasn't expired. If invalid, it returns an error.
    Password Update: If valid, the new password is hashed using bcrypt, and the token fields are cleared from the user's record.
    Email Notification: A password reset success email is sent to the user.
    Response: A success message is returned.

--- server/controller/user.controller.ts
+++ server/controller/user.controller.ts
@@ -149,6 +149,35 @@ export const forgotPassword = async (req: Request, res: Response) => {
         return res.status(500).json({ message: "Internal server error" });
     }
 };
+export const resetPassword = async (req: Request, res: Response) => {
+    try {
+        const { token } = req.params;
+        const { newPassword } = req.body;
+        const user = await User.findOne({ resetPasswordToken: token, resetPasswordTokenExpiresAt: { $gt: Date.now() } });
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid or expired reset token"
+            });
+        }
+        //update password
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+        user.password = hashedPassword;
+        user.resetPasswordToken = undefined;
+        user.resetPasswordTokenExpiresAt = undefined;
+        await user.save();
 
+        // send success reset email
+        await sendResetSuccessEmail(user.email);
+
+        return res.status(200).json({
+            success: true,
+            message: "Password reset successfully."
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
 
 
