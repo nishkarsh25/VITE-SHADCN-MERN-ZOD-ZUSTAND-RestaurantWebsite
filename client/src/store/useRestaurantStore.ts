a68507de6d27fe306bd65c1e2efb177eac298d06
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 02:52:13 +0530

    Imports: axios: A popular library used for making HTTP requests to a backend API. Here, it's configured to always send cookies along with the request (axios.defaults.withCredentials = true), which is important for handling authentication or session management. toast from sonner: A notification library that displays success or error messages to users after API operations. It enhances the user experience by providing immediate feedback. create from zustand: Zustand’s function that allows creating the state management store. createJSONStorage, persist from zustand/middleware: Middleware that enables persistence of the store state, in this case, using localStorage to ensure the state is retained across sessions.

---
+++ client/src/store/useRestaurantStore.ts
@@ -0,0 +1,10 @@
+import { Orders } from "@/types/orderType";
+import { MenuItem, RestaurantState } from "@/types/restaurantType";
+import axios from "axios";
+import { toast } from "sonner";
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+
+
+
