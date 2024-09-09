2edd5dd10858ef0dad4c3ad948b4aacffd77da2f
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 04:42:47 +0530

    Imports Components and Pages:
    Login, Signup, ForgotPassword, ResetPassword, VerifyEmail: Authentication-related components.
    HereSection, Profile, SearchPage, RestaurantDetail, Cart, Success: Various application pages and components.
    Restaurant, AddMenu, Orders: Admin-related components.
    Routing:
    
    createBrowserRouter, RouterProvider: Functions from react-router-dom to set up routing.
    State Management:
    
    useUserStore: Custom hook for user-related state and actions.
    useThemeStore: Custom hook for theme management.
    Utilities:
    
    Navigate: Component from react-router-dom for programmatic navigation.
    useEffect: React hook for handling side effects.
    Components:
    
    Loading: Component to show a loading state while authentication or theme initialization is in progress.

---
+++ client/src/App.tsx
@@ -0,0 +1,33 @@
+import Login from "./auth/Login";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Signup from "./auth/Signup";
+import ForgotPassword from "./auth/ForgotPassword";
+import ResetPassword from "./auth/ResetPassword";
+import VerifyEmail from "./auth/VerifyEmail";
+import HereSection from "./components/HereSection";
+import MainLayout from "./layout/MainLayout";
+import Profile from "./components/Profile";
+import SearchPage from "./components/SearchPage";
+import RestaurantDetail from "./components/RestaurantDetail";
+import Cart from "./components/Cart";
+import Restaurant from "./admin/Restaurant";
+import AddMenu from "./admin/AddMenu";
+import Orders from "./admin/Orders";
+import Success from "./components/Success";
+import { useUserStore } from "./store/useUserStore";
+import { Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import Loading from "./components/Loading";
+import { useThemeStore } from "./store/useThemeStore";
+
+
+
+
+
+
+
+
+
+
+
+
