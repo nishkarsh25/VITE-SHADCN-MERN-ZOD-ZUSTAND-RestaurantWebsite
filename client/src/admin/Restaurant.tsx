c4fe4dff9c0cba703c06e5a5d5cc3ed9f72f937b
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 03:42:50 +0530

    Imports: UI Components:
    Button: A custom button component used for form submission and displaying loading state.
    Input: A custom input component for various types of user inputs.
    Label: A custom label component for form field labels.
    Schema:
    
    RestaurantFormSchema: TypeScript interface or type that defines the shape of the form data.
    restaurantFromSchema: Schema used for validation of the form data.
    State Management:
    
    useRestaurantStore: Custom hook to interact with the restaurant store, which provides functions and state related to restaurant operations.
    Icons:
    
    Loader2: A loading spinner icon from lucide-react, used to indicate a loading state.
    React Hooks:
    
    useEffect: React hook used to perform side effects such as fetching data when the component mounts.
    useState: React hook for managing component state.

---
+++ client/src/admin/Restaurant.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  RestaurantFormSchema,
+  restaurantFromSchema,
+} from "@/schema/restaurantSchema";
+import { useRestaurantStore } from "@/store/useRestaurantStore";
+import { Loader2 } from "lucide-react";
+import { FormEvent, useEffect, useState } from "react";
+
+
+
+
