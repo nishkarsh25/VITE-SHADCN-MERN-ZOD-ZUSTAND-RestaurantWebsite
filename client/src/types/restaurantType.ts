845899b2c62d920b8489e775e4f0773a1fe71130
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 03:05:51 +0530

    Restaurant Type:
    Represents a restaurant and its properties.
    Properties:
    _id: Unique identifier for the restaurant.
    user: Identifier for the user (presumably the owner or manager) associated with the restaurant.
    restaurantName: Name of the restaurant.
    city: The city where the restaurant is located.
    country: The country where the restaurant is located.
    deliveryTime: Estimated time for delivering orders from this restaurant.
    cuisines: An array of strings representing the different cuisines the restaurant offers.
    menus: An array of MenuItem objects representing the menu items offered by the restaurant.
    imageUrl: URL of the image representing the restaurant.

--- client/src/types/restaurantType.ts
+++ client/src/types/restaurantType.ts
@@ -7,7 +7,17 @@ export type MenuItem = {
     price: number;
     image: string;
 }
-
+export type Restaurant = {
+    _id: string;
+    user: string;
+    restaurantName: string;
+    city: string;
+    country: string;
+    deliveryTime: number;
+    cuisines: string[];
+    menus: MenuItem[];
+    imageUrl: string;
+}
 
 
 
