d0d119ed2ac513734102d56a02f2fd6e2f1c246c
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 03:43:04 +0530

    Component Definition: State Management:
    input: A state variable holding the form data. It includes fields for restaurant details like name, city, country, delivery time, cuisines, and image file.
    setInput: Function to update the input state when form fields change.
    errors: A state variable holding validation errors for each form field.
    setErrors: Function to update the errors state based on validation results.

--- client/src/admin/Restaurant.tsx
+++ client/src/admin/Restaurant.tsx
@@ -9,6 +9,30 @@ import { useRestaurantStore } from "@/store/useRestaurantStore";
 import { Loader2 } from "lucide-react";
 import { FormEvent, useEffect, useState } from "react";
 
+const Restaurant = () => {
+  const [input, setInput] = useState<RestaurantFormSchema>({
+    restaurantName: "",
+    city: "",
+    country: "",
+    deliveryTime: 0,
+    cuisines: [],
+    imageFile: undefined,
+  });
+  const [errors, setErrors] = useState<Partial<RestaurantFormSchema>>({});
+  const {
+    loading,
+    restaurant,
+    updateRestaurant,
+    createRestaurant,
+    getRestaurant,
+  } = useRestaurantStore();
 
+  
+
+ 
+  
+
+  
+};
 
 
