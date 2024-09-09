65543ba7d3acebaeab8eab6639bc3524c8e0ae3e
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-09 23:10:07 +0530

    getOrders Function Purpose: Retrieves all orders for the current user. Process: Query Orders: Finds orders in the database where the user field matches the current user’s ID (req.id), and populates the associated user and restaurant data. Response: Returns a JSON response with a success status and the list of orders. If an error occurs, it logs the error and returns a 500 status code with an error message.

--- server/controller/order.controller.ts
+++ server/controller/order.controller.ts
@@ -3,11 +3,37 @@ import { Restaurant } from "../models/restaurant.model";
 import { Order } from "../models/order.model";
 import Stripe from "stripe";
 
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
+type CheckoutSessionRequest = {
+    cartItems: {
+        menuId: string;
+        name: string;
+        image: string;
+        price: number;
+        quantity: number
+    }[],
+    deliveryDetails: {
+        name: string;
+        email: string;
+        address: string;
+        city: string
+    },
+    restaurantId: string
+}
 
-
-
-
+export const getOrders = async (req: Request, res: Response) => {
+    try {
+        const orders = await Order.find({ user: req.id }).populate('user').populate('restaurant');
+        return res.status(200).json({
+            success: true,
+            orders
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ success: false, message: "Internal server error" });
+    }
+}
 
 
 
