4d3323d01fd1213abacfa5b81a48ee7386c292da
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-09 23:12:13 +0530

    Router Creation const router = express.Router();: Creates an instance of an Express router. This router will handle HTTP requests for the user-related routes.

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { checkAuth, forgotPassword, login, logout, resetPassword, signup, updateProfile, verifyEmail } from "../controller/user.controller";
 import { isAuthenticated } from "../middlewares/isAuthenticated";
 
-
+const router = express.Router();
 
 
 
