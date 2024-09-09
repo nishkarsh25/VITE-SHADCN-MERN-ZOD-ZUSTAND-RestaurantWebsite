56442164011d7d3531393c32b09285a9cf154bc5
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 02:57:54 +0530

    Imports: axios: Used for making HTTP requests to the backend API. The withCredentials flag is set to ensure that cookies (for authentication) are sent with requests. toast from sonner: This library is used to show success or error notifications to the user based on API responses. create from zustand: Used to create the Zustand store for managing the state of the menu-related operations. createJSONStorage and persist from zustand/middleware: Middleware functions that allow the store's state to be persisted in localStorage. useRestaurantStore: This store (from another file) manages restaurant data. It’s used here to update the restaurant’s menu whenever a new menu is created or edited.

---
+++ client/src/store/useMenuStore.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+import { toast } from "sonner";
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import { useRestaurantStore } from "./useRestaurantStore";
+
+
+
+
+
+
