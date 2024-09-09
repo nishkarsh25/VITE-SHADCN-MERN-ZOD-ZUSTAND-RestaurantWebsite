ff9760adb762b9c430f08db5eb4eb0771316029f
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 04:33:29 +0530

    Imports MenuItem:
    A TypeScript type that defines the structure of a menu item, including properties like name, description, price, and image.
    Button:
    
    A UI component for creating buttons. It’s used here to provide functionality for adding menu items to the cart.
    Card, CardContent, CardFooter:
    
    UI components for displaying content in card format. They are used to style and organize the menu item details.
    useCartStore:
    
    A custom hook that provides access to the global cart state. It includes methods like addToCart to manage cart operations.
    useNavigate:
    
    A hook from react-router-dom that allows programmatic navigation to different routes.

---
+++ client/src/components/AvailableMenu.tsx
@@ -0,0 +1,9 @@
+import { MenuItem } from "@/types/restaurantType";
+import { Button } from "./ui/button";
+import { Card, CardContent, CardFooter } from "./ui/card";
+import { useCartStore } from "@/store/useCartStore";
+import { useNavigate } from "react-router-dom";
+
+
+
+
