348400e207803495e1d6e370c87be89d75cd7656
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 02:49:47 +0530

    API Endpoint Configuration: API_END_POINT: This constant defines the base URL for the user-related API routes. All HTTP requests in the store use this endpoint to interact with the backend. axios.defaults.withCredentials = true: This ensures that any request made using Axios will send cookies (such as session tokens) along with the request, which is important for authentication.

--- client/src/store/useUserStore.ts
+++ client/src/store/useUserStore.ts
@@ -4,7 +4,8 @@ import axios from "axios";
 import { LoginInputState, SignupInputState } from "@/schema/userSchema";
 import { toast } from "sonner";
 
-
+const API_END_POINT = "http://localhost:8000/api/v1/user"
+axios.defaults.withCredentials = true;
 
 
 
