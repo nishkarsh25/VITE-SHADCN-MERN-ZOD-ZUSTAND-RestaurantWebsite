a541724c3d2f4827ad7fb82a7b001f203d3a02d8
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-09 23:17:23 +0530

    Middleware app.use(bodyParser.json({ limit: '10mb' }));: Configures the body parser middleware to handle JSON payloads with a maximum size of 10mb.
    app.use(express.urlencoded({ extended: true, limit: '10mb' }));: Configures Express to handle URL-encoded data with a maximum size of 10mb.
    
    app.use(express.json());: Configures Express to handle JSON payloads.
    
    app.use(cookieParser());: Parses cookies from request headers.
    
    const corsOptions = { origin: "http://localhost:5173", credentials: true };: Defines CORS options to allow requests from http://localhost:5173 and include credentials (such as cookies) in the requests.
    
    app.use(cors(corsOptions));: Configures CORS middleware with the specified options.

--- server/index.ts
+++ server/index.ts
@@ -18,7 +18,16 @@ const PORT = process.env.PORT || 3000;
 
 const DIRNAME = path.resolve();
 
-
+// default middleware for any mern project
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json());
+app.use(cookieParser());
+const corsOptions = {
+    origin: "http://localhost:5173",
+    credentials: true
+}
+app.use(cors(corsOptions));
 
 
 
