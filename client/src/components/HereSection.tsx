5e766b721ca50b0e8d249d7df2cabd94495c4a6e
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 04:16:10 +0530

    Imports: React Hooks:
    useState: For managing the local state of the search input text.
    useNavigate: From react-router-dom, for programmatic navigation to different routes.
    UI Components:
    
    Input: A custom input component used for the search bar.
    Search: An icon from lucide-react, used as a visual indicator for the search functionality.
    Button: A custom button component used to trigger the search action.
    Assets:
    
    HereImage: An image imported from the assets folder, presumably used for visual enhancement in this section.

---
+++ client/src/components/HereSection.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { Input } from "./ui/input";
+import { Search } from "lucide-react";
+import { Button } from "./ui/button";
+import HereImage from "@/assets/hero_pizza.png";
+import { useNavigate } from "react-router-dom";
+
+
+
+
