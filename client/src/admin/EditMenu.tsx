355df599c0a9163a13d02faf0431162acf21c0c9
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 04:02:32 +0530

    EditMenu Component: The EditMenu component is a React functional component designed to handle the editing of menu items in a restaurant. It provides a user interface for modifying the details of a selected menu item. Here’s a comprehensive breakdown of each part of the component:
    Props:
    selectedMenu: An object representing the menu item currently selected for editing. It includes properties such as name, description, price, and image.
    editOpen: A boolean state indicating whether the edit dialog is open or closed.
    setEditOpen: A function to update the editOpen state, used to control the visibility of the dialog.
    State Management:
    input: An object holding the current values of the menu item being edited. It includes fields for name, description, price, and image. It is initialized with default values.
    setInput: A function used to update the input state when the user modifies form fields.
    error: An object holding validation errors for the form fields. This is used to display error messages if the form data is invalid.
    setError: A function to update the error state with validation errors.

---
+++ client/src/admin/EditMenu.tsx
@@ -0,0 +1,48 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { MenuFormSchema, menuSchema } from "@/schema/menuSchema";
+import { useMenuStore } from "@/store/useMenuStore";
+import { MenuItem } from "@/types/restaurantType";
+import { Loader2 } from "lucide-react";
+import {
+  Dispatch,
+  FormEvent,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
+
+const EditMenu = ({
+  selectedMenu,
+  editOpen,
+  setEditOpen,
+}: {
+  selectedMenu: MenuItem;
+  editOpen: boolean;
+  setEditOpen: Dispatch<SetStateAction<boolean>>;
+}) => {
+  const [input, setInput] = useState<MenuFormSchema>({
+    name: "",
+    description: "",
+    price: 0,
+    image: undefined,
+  });
+  const [error, setError] = useState<Partial<MenuFormSchema>>({});
+  const {loading, editMenu} = useMenuStore();
+
+  
+
+  
+  
+};
+
+
