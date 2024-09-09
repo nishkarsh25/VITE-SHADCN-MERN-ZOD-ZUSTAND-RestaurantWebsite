c790013493b6008f9ea8bae180fec1b1568d287e
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-09 23:13:39 +0530

    Route Definitions / (POST):
    Middleware: isAuthenticated (ensures the user is logged in before accessing this route)
    File Upload Middleware: upload.single("imageFile") (handles single file uploads with the field name imageFile)
    Controller Function: createRestaurant (handles creating a new restaurant)
    / (GET):
    
    Middleware: isAuthenticated (ensures the user is logged in before accessing this route)
    Controller Function: getRestaurant (retrieves the restaurant details for the authenticated user)
    / (PUT):
    
    Middleware: isAuthenticated (ensures the user is logged in before accessing this route)
    File Upload Middleware: upload.single("imageFile") (handles single file uploads with the field name imageFile)
    Controller Function: updateRestaurant (handles updating restaurant details)
    /order (GET):
    
    Middleware: isAuthenticated (ensures the user is logged in before accessing this route)
    Controller Function: getRestaurantOrder (retrieves orders for the restaurant associated with the authenticated user)
    /order/:orderId/status (PUT):
    
    Middleware: isAuthenticated (ensures the user is logged in before accessing this route)
    Controller Function: updateOrderStatus (updates the status of a specific order based on orderId)
    /search/:searchText (GET):
    
    Middleware: isAuthenticated (ensures the user is logged in before accessing this route)
    Controller Function: searchRestaurant (searches for restaurants based on searchText)
    /:id (GET):
    
    Middleware: isAuthenticated (ensures the user is logged in before accessing this route)
    Controller Function: getSingleRestaurant (retrieves details for a specific restaurant based on id)

--- server/routes/restaurant.route.ts
+++ server/routes/restaurant.route.ts
@@ -5,7 +5,13 @@ import {isAuthenticated} from "../middlewares/isAuthenticated";
 
 const router = express.Router();
 
-
+router.route("/").post(isAuthenticated, upload.single("imageFile"), createRestaurant);
+router.route("/").get(isAuthenticated, getRestaurant);
+router.route("/").put(isAuthenticated, upload.single("imageFile"), updateRestaurant);
+router.route("/order").get(isAuthenticated,  getRestaurantOrder);
+router.route("/order/:orderId/status").put(isAuthenticated, updateOrderStatus);
+router.route("/search/:searchText").get(isAuthenticated, searchRestaurant);
+router.route("/:id").get(isAuthenticated, getSingleRestaurant);
 
 
 
