661d856d1235e695f05972fbe70d7bf6748a5f21
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 04:38:12 +0530

    Handlers:
    changeEventHandler:
    
    Updates the input state when a form field changes. It captures the name and value from the input element and updates the corresponding field in the input state object.
    checkoutHandler:
    
    Handles form submission:
    Prevents default form behavior.
    Constructs a checkoutData object with the current cart items and user input.
    Calls createCheckoutSession from the useOrderStore to process the checkout. Logs errors if any occur during this process.

--- client/src/components/CheckoutConfirmPage.tsx
+++ client/src/components/CheckoutConfirmPage.tsx
@@ -35,7 +35,30 @@ const CheckoutConfirmPage = ({
   const { cart } = useCartStore();
   const { restaurant } = useRestaurantStore();
   const { createCheckoutSession, loading } = useOrderStore();
-  
+  const changeEventHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInput({ ...input, [name]: value });
+  };
+  const checkoutHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // api implementation start from here
+    try {
+      const checkoutData: CheckoutSessionRequest = {
+        cartItems: cart.map((cartItem) => ({
+          menuId: cartItem._id,
+          name: cartItem.name,
+          image: cartItem.image,
+          price: cartItem.price.toString(),
+          quantity: cartItem.quantity.toString(),
+        })),
+        deliveryDetails: input,
+        restaurantId: restaurant?._id as string,
+      };
+      await createCheckoutSession(checkoutData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   
 };
