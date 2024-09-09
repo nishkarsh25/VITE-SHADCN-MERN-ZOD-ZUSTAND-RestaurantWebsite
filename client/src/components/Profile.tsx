919d1d612b3676e1c44ae13e1ed8511fd5b8ccfc
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 04:21:33 +0530

    Component Breakdown: State Management:
    isLoading: A boolean state to track whether the profile update process is ongoing. It’s used to show a loading indicator while the profile is being updated.
    profileData: An object state holding the profile information. It initializes with the current user’s data or empty strings if no data is available.
    selectedProfilePicture: A state to manage the preview of the selected profile picture before it’s uploaded.
    Refs:
    
    imageRef: A reference to the hidden file input element used to select a new profile picture.

---
+++ client/src/components/Profile.tsx
@@ -0,0 +1,36 @@
+import {
+  Loader2,
+  LocateIcon,
+  Mail,
+  MapPin ,
+  MapPinnedIcon,
+  Plus,
+} from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { FormEvent, useRef, useState } from "react";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { Button } from "./ui/button";
+import { useUserStore } from "@/store/useUserStore";
+
+const Profile = () => {
+  const {user, updateProfile} = useUserStore();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [profileData, setProfileData] = useState({
+    fullname: user?.fullname || "",
+    email: user?.email || "", 
+    address: user?.address || "",
+    city: user?.city || "",
+    country: user?.country || "",
+    profilePicture: user?.profilePicture || "",
+  });
+  const imageRef = useRef<HTMLInputElement | null>(null);
+  const [selectedProfilePicture, setSelectedProfilePicture] =
+    useState<string>( profileData.profilePicture || "");
+ 
+  
+
+  
+};
+
+
