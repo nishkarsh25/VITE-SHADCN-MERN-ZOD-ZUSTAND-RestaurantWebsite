7b327f73e496bc6a71f6f3a5107b7b0dff1db3d9
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 02:55:52 +0530

    API Endpoint Configuration: API_END_POINT: A constant that stores the base URL for all order-related API requests (http://localhost:8000/api/v1/order).

--- client/src/store/useOrderStore.ts
+++ client/src/store/useOrderStore.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
-
+const API_END_POINT: string = "http://localhost:8000/api/v1/order";
+axios.defaults.withCredentials = true;
 
 
