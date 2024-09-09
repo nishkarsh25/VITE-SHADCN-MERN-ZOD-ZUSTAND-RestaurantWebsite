0a276429d79fe9951f3cb37b9e07538c86f45e14
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 03:02:00 +0530

    ThemeStore: This is the structure of the store's state, containing:
    theme: A Theme type value representing the current theme (either "dark" or "light").
    setTheme: A function to change the current theme.
    loadThemeFromStorage: A function to load the theme from localStorage or set a default theme.
    initializeTheme: A function to initialize the theme based on what is stored in localStorage.

--- client/src/store/useThemeStore.ts
+++ client/src/store/useThemeStore.ts
@@ -3,5 +3,9 @@ import { createJSONStorage, persist } from "zustand/middleware";
 
 export type Theme = "dark" | "light";
 
-
+type ThemeStore = {
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
+  loadThemeFromStorage: (storageKey: string, defaultTheme: Theme) => void;
+};
 
