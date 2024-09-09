bb4f35c06c00d47015b35bae5ef065ce3e326a46
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-09 19:23:29 +0530

    Connecting to MongoDB: Inside the function, the mongoose.connect method is called with an argument that retrieves the MongoDB URI from the environment variables. This URI is typically stored as MONGO_URI in the environment configuration (.env file) to keep sensitive database credentials secure.
    The connect method establishes a connection to the MongoDB instance using the provided URI. The await keyword ensures that the function waits for the connection process to complete before moving forward.
    
    4. Success Message:
    If the connection is successful, a message (mongoDB connected.) is printed to the console, indicating that the application has successfully connected to the database.

--- server/db/connectDB.ts
+++ server/db/connectDB.ts
@@ -2,5 +2,10 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-   
+    try {
+        await mongoose.connect(process.env.MONGO_URI!);
+        console.log('mongoDB connected.');
+    } catch (error) {
+        
+    }
 }
