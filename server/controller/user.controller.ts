061ef65fda72d503b5f6cdb76552d1d13dacbf60
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-09 23:00:10 +0530

    Signup Function: This function handles user registration.
    Input Validation and User Lookup: The function extracts user details (e.g., name, email, password, contact) from the request body and checks if a user already exists with the provided email. If a user exists, it returns an error response.
    Password Hashing and Token Generation: If the email is unique, the password is hashed using bcrypt for secure storage, and a verification token is generated.
    User Creation: The user object is created in the database with the hashed password, verification token, and token expiration time.
    Token and Email: An authentication token is generated and sent back to the client, and a verification email is sent to the user's email.
    Response: The response includes user data (excluding the password) and a success message.

--- server/controller/user.controller.ts
+++ server/controller/user.controller.ts
@@ -7,7 +7,43 @@ import { generateVerificationCode } from "../utils/generateVerificationCode";
 import { generateToken } from "../utils/generateToken";
 import { sendPasswordResetEmail, sendResetSuccessEmail, sendVerificationEmail, sendWelcomeEmail } from "../mailtrap/email";
 
+export const signup = async (req: Request, res: Response) => {
+    try {
+        const { fullname, email, password, contact } = req.body;
 
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({
+                success: false,
+                message: "User already exist with this email"
+            })
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const verificationToken =  generateVerificationCode();
+
+        user = await User.create({
+            fullname,
+            email,
+            password: hashedPassword,
+            contact: Number(contact),
+            verificationToken,
+            verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000,
+        })
+        generateToken(res,user);
+
+        await sendVerificationEmail(email, verificationToken);
+
+        const userWithoutPassword = await User.findOne({ email }).select("-password");
+        return res.status(201).json({
+            success: true,
+            message: "Account created successfully",
+            user: userWithoutPassword
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal server error" })
+    }
+};
 
 
 
