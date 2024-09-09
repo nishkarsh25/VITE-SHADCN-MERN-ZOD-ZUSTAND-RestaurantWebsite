06dcb391ec5a5ce0a92abec2c5d10433200bdfe4
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 02:55:41 +0530

    Imports: axios: This is a library used to make HTTP requests to the backend. Here it is configured to send cookies (axios.defaults.withCredentials = true), which is useful for authentication. create from zustand: Zustand’s function used to create a store for managing the application state. createJSONStorage, persist from zustand/middleware: Middleware that allows the store to persist its state in localStorage, ensuring that state is saved across page reloads.

---
+++ client/src/store/useOrderStore.ts
@@ -0,0 +1,8 @@
+import { CheckoutSessionRequest, OrderState } from "@/types/orderType";
+import axios from "axios";
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+
+
+
