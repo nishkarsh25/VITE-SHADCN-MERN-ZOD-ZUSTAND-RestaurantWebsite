c21c07d16ae1747d0662160d2a08fbfd22a83bc5
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-10 04:39:16 +0530

    Imports UI Components and Icons:
    IndianRupee:
    An icon from lucide-react used to represent the currency symbol.
    Separator:
    A UI component used to add visual separation between items.
    Link:
    From react-router-dom, used to create navigation links.
    Button:
    A UI component for creating buttons.

---
+++ client/src/components/Success.tsx
@@ -0,0 +1,12 @@
+ 
+import { IndianRupee } from "lucide-react";
+import { Separator } from "./ui/separator";
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+import { useOrderStore } from "@/store/useOrderStore";
+import { useEffect } from "react"; 
+import { CartItem } from "@/types/cartType";
+
+
+
+
