68bfb4e497edf31bec04c811cb6b54f09cda1367
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-09-09 22:52:34 +0530

    Retrieving the Token: const token = req.cookies.token;: The function retrieves the JWT from the token cookie of the incoming request. If the token is not present, the function responds with a 401 Unauthorized status, indicating that the user is not authenticated. 4. Verifying the Token: const decode = jwt.verify(token, process.env.SECRET_KEY!) as jwt.JwtPayload;: The jwt.verify method is used to verify the validity of the JWT using a secret key stored in environment variables. The process.env.SECRET_KEY! ensures that the secret key is retrieved from environment variables and is non-null. The result of jwt.verify is cast to jwt.JwtPayload, which contains the payload of the token (such as user ID). 5. Handling Verification Errors: if (!decode): After attempting to verify the token, it checks if the token decoding was successful. If the token is invalid or the verification process fails, it responds with a 401 Unauthorized status, indicating an invalid token. 6. Attaching User ID to Request: req.id = decode.userId;: If the token is successfully verified, the user ID extracted from the token is attached to the request object as req.id. This allows subsequent middleware functions or route handlers to access the user ID and perform actions based on the authenticated user. 7. Proceeding to Next Middleware: next();: Calls the next function to pass control to the next middleware or route handler in the stack. This allows the request to continue through the rest of the middleware chain or reach the desired route handler.

--- server/middlewares/isAuthenticated.ts
+++ server/middlewares/isAuthenticated.ts
@@ -10,5 +10,26 @@ declare global {
 }
 
 export const isAuthenticated = async (req: Request, res: Response, next: NextFunction) => {
-    
+    try {
+        const token = req.cookies.token;
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message: "User not authenticated"
+            });
+        }
+        // verify the toekn
+        const decode = jwt.verify(token, process.env.SECRET_KEY!) as jwt.JwtPayload;
+        // check is decoding was successfull
+        if (!decode) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid token"
+            })
+        }
+        req.id = decode.userId;
+        next();
+    } catch (error) {
+        
+    }
 }
